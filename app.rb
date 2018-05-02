4db4231452aa6b41fcd362a61db5cf74d5b0ee59
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-02 14:05:04 +0100

    Add attack button

--- app.rb
+++ app.rb
@@ -18,7 +18,13 @@ class UBM < Sinatra::Base
     @player2 = session[:player2]
     @p1_health = 100
     @p2_health = 100
+    @attack_message = session[:attack_message]
     erb :play
+  end
+
+  post '/attack' do
+    session[:attack_message] = 'Player 1 has slapped Player 2!'
+    redirect '/play'
   end
 
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -12,3 +12,11 @@ feature 'Testing infrastructure' do
   end
 
 end
+
+feature 'Testing battle mechanics' do
+  scenario 'Player 1 attacks player 2 and receives confirmation' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content 'Player 1 has slapped Player 2!'
+  end
+end
